8ab28169ee9f396fe7608c6dcb291ebb0094a3c3
Krishnendu-Ghosh-Web <bkrishnendughosh@gmail.com>
2024-06-20 12:40:00 +0530

    compliete

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import styles from"./App.module.css"
+import Display from './components/Display'
+import ButtonContainer from './components/ButtonContainer'
+function App() {
+
+  const [calVal,setCalVal]=useState("");
+  const onButtonClick=(buttonText)=>{
+   if(buttonText==='c')
+    {
+    setCalVal("");
+   }
+   else if(buttonText==='=')
+    {
+const result= eval(calVal);
+setCalVal(result);
+   }
+   else
+   {
+    const newDisplayValue=calVal+ buttonText;
+    setCalVal(newDisplayValue);
+   }
+  }
+  return (
+    <div style={{display:'flex',justifyContent:'center',width:'100vw'}}>
+    <center style={{display:'flex',flexDirection:'column',justifyContent:'center'}} className={styles.calculator}>
+<Display displayValue={calVal}></Display>
+<ButtonContainer onButtonClick={onButtonClick} ></ButtonContainer>
+    </center>
+    </div>
+  )
+}
+
+export default App
